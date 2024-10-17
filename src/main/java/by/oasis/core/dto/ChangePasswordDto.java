68aafd295ae2862765901e9a99982c15ab42df08
MSAnd <hh@gmail.com>
2024-10-17 18:58:27 +0300

    Изменение пароля пользователем

---
+++ src/main/java/by/oasis/core/dto/ChangePasswordDto.java
@@ -0,0 +1,14 @@
+package by.oasis.core.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ChangePassword {
+    public String currentPassword;
+    public String newPassword;
+    public String 
+}
